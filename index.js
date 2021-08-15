8a0a22b4b0f9347d5da8198c63312ff0d952aec0
thekonly <joaopedro_m@icloud.com>
2021-08-14 20:39:37 -0400

    Assignment Completed

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a,b){
+    return a+b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+function increment(a){
+    return ++a;
+}
+
+function decrement(number){
+    number-- ;
+    return number;
+}
+
+function makeInt(n) {
+    parseInt(n);
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    parseFloat(n);
+    return parseFloat(n);
+}
